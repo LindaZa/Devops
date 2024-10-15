0df04656817953bd2dae95be0976e93588b02b26
LindaZa <lindayasmine.zaafrani@esprit.tn>
2024-10-15 12:09:15 +0100

    First commit with Maven and LOG4

---
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -0,0 +1,83 @@
+package tn.esprit.spring.kaddem.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
+
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/etudiant")
+public class EtudiantRestController {
+	@Autowired
+	IEtudiantService etudiantService;
+	private static final Logger logger = LogManager.getLogger(EtudiantRestController.class);
+
+	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
+	@GetMapping("/retrieve-all-etudiants")
+	public List<Etudiant> getEtudiants() {
+		logger.info("Fetching all students");
+		List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
+		return listEtudiants;
+	}
+	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
+	@GetMapping("/retrieve-etudiant/{etudiant-id}")
+	public Etudiant retrieveEtudiant(@PathVariable("etudiant-id") Integer etudiantId) {
+		logger.info("Retrieving student with ID: {}", etudiantId);
+		return etudiantService.retrieveEtudiant(etudiantId);
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/add-etudiant
+	@PostMapping("/add-etudiant")
+	public Etudiant addEtudiant(@RequestBody Etudiant e) {
+		logger.info("Adding new student: {}", e);
+		Etudiant etudiant = etudiantService.addEtudiant(e);
+		return etudiant;
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
+	@DeleteMapping("/remove-etudiant/{etudiant-id}")
+	public void removeEtudiant(@PathVariable("etudiant-id") Integer etudiantId) {
+		logger.info("Removing student with ID: {}", etudiantId);
+		etudiantService.removeEtudiant(etudiantId);
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/update-etudiant
+	@PutMapping("/update-etudiant")
+	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
+		logger.info("Updating student: {}", e);
+		Etudiant etudiant= etudiantService.updateEtudiant(e);
+
+		return etudiant;
+	}
+
+	//@PutMapping("/affecter-etudiant-departement")
+	@PutMapping(value="/affecter-etudiant-departement/{etudiantId}/{departementId}")
+	public void affecterEtudiantToDepartement(@PathVariable("etudiantId") Integer etudiantId, @PathVariable("departementId")Integer departementId){
+		logger.info("Assigning student with ID: {} to department with ID: {}", etudiantId, departementId);
+		etudiantService.assignEtudiantToDepartement(etudiantId, departementId);
+    }
+//addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe)
+    /* Ajouter un étudiant tout en lui affectant un contrat et une équipe */
+    @PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
+    @ResponseBody
+    public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
+		logger.info("Adding student and assigning to contract ID: {} and team ID: {}", idContrat, idEquipe);
+        Etudiant etudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
+        return etudiant;
+    }
+
+	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
+	public List<Etudiant> getEtudiantsParDepartement(@PathVariable("idDepartement") Integer idDepartement) {
+		logger.info("Fetching students from department with ID: {}", idDepartement);
+		return etudiantService.getEtudiantsByDepartement(idDepartement);
+	}
+
+}
+
+
